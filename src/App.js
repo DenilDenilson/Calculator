3e4917a11558b9f3310ccfd1d4a8727bef522986
DenilDenilson <jordanpreseleccion@gmail.com>
2022-08-16 02:09:32 -0500

    Pantalla maquetada

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import Screen from './components/Screen';
 
 function App() {
   return (
     <div className="App">
       {/* Barra superior */}
       <NavBar marca='calc'></NavBar>
-
+      <Screen></Screen>
     </div>
   );
 }
---
+++ src/components/Screen.jsx
@@ -0,0 +1,7 @@
+export default function Sreen() {
+  return (
+    <div>
+      Valores
+    </div>
+  )
+}
