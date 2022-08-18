7b2a2ed664e3ebd04c9747510923cd2b0edff5c8
DenilDenilson <jordanpreseleccion@gmail.com>
2022-08-18 04:01:42 -0500

    Added the LocaleString in en-Us for the numbers

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App w-screen h-screen bg-VDDB_main_background grid place-content-center">
       {/* Barra superior */}
-      <section className="w-[22rem] h-[36rem]">
+      <section className="w-[21rem] h-[36rem]">
         <NavBar marca='calc'></NavBar>
         <Screen></Screen>
         <Board></Board>
--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,7 +1,9 @@
 export default function Sreen() {
+  const number = parseInt('51254125')
+  let number_display = number.toLocaleString('en-US')
   return (
     <div className="text-White bg-VDDB_screen_background h-[4.5rem] font-sans my-4 rounded-lg flex items-center justify-end text-3xl p-5">
-      65165131352
+      {number_display}
     </div>
   )
 }
